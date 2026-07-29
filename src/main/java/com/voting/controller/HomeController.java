0f1352dfcc3e3285a6857b5fa34c9b70dd669090
ahmedzakir8901 <ahmedzakir8901@gmail.com>
2026-07-29 05:49:37 +0500

    done nibba now working fine

--- src/main/java/com/voting/controller/HomeController.java
+++ src/main/java/com/voting/controller/HomeController.java
@@ -1,12 +1,13 @@
 package com.voting.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-    @Controller
-    public class HomeController {
-        @GetMapping("/index")
-        public String index() {
-            return "index";
-        }
-    }
+@Controller
+public class HomeController {
 
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
