f18d4beef83f5ca2f532034d5f86fba260d49fb1
ahmedzakir8901 <ahmedzakir8901@gmail.com>
2026-08-01 04:36:50 +0500

    entities package done nigga

---
+++ src/main/java/com/voting/entity/User.java
@@ -0,0 +1,94 @@
+package com.voting.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            length = 100
+    )
+    private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false,
+            length = 100
+    )
+    private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            unique = true,
+            length = 255
+    )
+    private String email;
+
+    @Column(
+            name = "password",
+            nullable = false,
+            length = 255
+    )
+    private String password;
+
+    @Column(
+            name = "cnic",
+            nullable = false,
+            unique = true,
+            length = 15
+    )
+    private String cnic;
+
+    @Column(
+            name = "phone_number",
+            nullable = false,
+            unique = true,
+            length = 15
+    )
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "role_id",
+            nullable = false
+    )
+    private Role role;
+
+    @CreationTimestamp
+    @Column(
+            name = "created_at",
+            nullable = false,
+            updatable = false
+    )
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(
+            name = "updated_at"
+    )
+    private LocalDateTime updatedAt;
+
+    @Column(
+            name = "status",
+            nullable = false
+    )
+    @Builder.Default
+    private Boolean status = true;
+}
